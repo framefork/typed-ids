22dbf86827a34548f7e7c391458d97c4837fd414
Filip Prochazka <dev@fprochazka.cz>
2024-11-24 17:32:18 +0100

    buildSrc: set jvm target to 21

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,3 +13,9 @@ dependencies {
     implementation("net.ltgt.gradle:gradle-errorprone-plugin:4.1.0")
     implementation("net.ltgt.gradle:gradle-nullaway-plugin:2.1.0")
 }
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21 // This option specifies the target version of the generated JVM bytecode
+    }
+}
