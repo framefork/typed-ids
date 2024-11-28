8a7ab0637079d84fefc19b6d2201ae44699b0aa3
Filip Procházka <filip@prochazka.su>
2024-11-28 15:12:34 +0100

    set java compatibility of buildSrc to 21

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,6 +14,11 @@ dependencies {
     implementation("net.ltgt.gradle:gradle-nullaway-plugin:2.1.0")
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     compilerOptions {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21 // This option specifies the target version of the generated JVM bytecode
